ca2232d7bfec2990104cd4b46ea727b884090392
acopar <andrej.copar@zoho.com>
2017-02-09 10:45:17 +0100

    Do not display smtp error.

--- orangecontrib/prototypes/widgets/owfacematch.py
+++ orangecontrib/prototypes/widgets/owfacematch.py
@@ -438,7 +438,7 @@ class OWFaceMatch(OWWidget):
             mailserver.quit()
         except Exception:
             self.send_image_later(image, email, name)
-            self.Error.smtp_error()
+            #self.Error.smtp_error()
 
     def send_image_later(self, image, email, name):
         if not isinstance(image, QPixmap):
